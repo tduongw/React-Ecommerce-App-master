11c8b4bc6fdd421b34627770fe597c8c2ef5ee48
tduongw <bangbang882002@gmail.com>
2024-08-05 17:23:57 +0700

    init project

---
+++ src/Data.jsx
@@ -0,0 +1,46 @@
+const DATA = [
+    {
+        id : 0,
+        title : "SHEIN LUNE Plus Size Women's Summer Casual Shirt Collar Jumpsuits",
+        price : 750,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/1.jpg"
+    },
+    {
+        id : 1,
+        title : "SHEIN LUNE Plus Size Lace Trim Flared Sleeves Jumpsuit With Shoes",
+        price : 800,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/2.jpg"
+    },
+    {
+        id : 2,
+        title : "MiniSHEIN EZwear Jumpsuits Plus Size Zipper Small Ruffles Solid Color Leisure",
+        price : 950,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/3.jpg"
+    },
+    {
+        id : 3,
+        title : "SHEIN EZwear Jumpsuits ",
+        price : 1050,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/4.jpg"
+    },
+    {
+        id : 4,
+        title : "SHEIN EZwear Small Ruffles Solid Color Leisure",
+        price : 1150,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/5.jpg"
+    },
+    {
+        id : 5,
+        title : "SHEIN EZwear Jumpsuits Leisure",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/6.jpg"
+    }
+]
+
+export default DATA;
